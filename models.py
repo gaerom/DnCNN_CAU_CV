006ed021cb1dda4df6a4ef4ebca2f6a6b608775d
bo-lim <leebolim0701@cau.ac.kr>
2023-05-25 12:30:51 +0900

    @bo-lim [FEATURE]add annotations of model

---
+++ models.py
@@ -0,0 +1,28 @@
+# importing
+
+# class
+
+# def __init__
+'''
+self
+num_of_channels
+num_of_layers
+
+make sequential layers
+'''
+
+# def forward
+'''
+self
+x
+
+return: x-out
+'''
+
+
+# MODEL Architecture
+# conv + relu
+
+# conv + bn + relu : 레이어 수-2 만큼 반복
+
+# conv
