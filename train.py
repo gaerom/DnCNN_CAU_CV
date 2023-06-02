3f9db359e737d097fd9a3608bb16ccc12379f622
bo-lim <leebolim0701@cau.ac.kr>
2023-06-02 14:31:33 +0900

    @bo-lim lr scheduler 추가

--- train.py
+++ train.py
@@ -5,12 +5,10 @@ import torch.optim as optim
 import time
 from torch.utils.data import DataLoader
 from models import DnCNN
+from utils import MSE_loss, get_lr_scheduler
 
-class MSE_loss(nn.modules.loss._Loss):
-    def __init__(self):
-        super(MSE_loss, self).__init__()
-    def forward(self, y, target):
-        return f.mse_loss(y, target)
+
+
 
 # from dataloader_utils import train, train_dataloader, test_dataloader
 def train_dataloader(train_dataset):
@@ -35,10 +33,12 @@ def test_dataloader(test_dataset):
     return test_dataloader
 
 def train(opt):
+    # Config 파일에서 가져오기
     lr = 0.01
-    n_epoch = 100
+    n_epoch = 500
     batch_size = 64
     PATH = './models/'
+    lr_scheduler_name = 'MultiStepLR'
 
     # Define dataset_type based on the configuration
     dataset_type = opt['datasets']['train']['dataset_type']
@@ -49,6 +49,7 @@ def train(opt):
             train_dataset = define_Dataset(dataset_opt)
             train_dataloader = train_dataloader(train_dataset)
         elif phase == 'test':
+            avg_psnr = 0.0
             test_dataset = define_Dataset(dataset_opt)
             test_dataloader = test_dataloader(test_dataset)
         else:
@@ -60,11 +61,12 @@ def train(opt):
             model.train()
             criterion = MSE_loss()
             optimizer = optim.Adam(model.parameters(), lr=lr)
+            lr_scheduler = get_lr_scheduler(lr_scheduler_name, optimizer=optimizer)
             for epoch in range(0, n_epoch):
                 epoch_loss = 0
                 st = time.time()
                 for xy in train_dataloader:
-                    x, y = xy[0].cuda(), xy[1].cuda()
+                    x, y = xy[0].cuda(), xy[1].cuda() # x: noisy image, y:ground truth
                     optimizer.zero_grad()
                     prediction = model(x)
                     loss = criterion(prediction, y)
@@ -72,7 +74,12 @@ def train(opt):
                     loss.backward()
                     optimizer.step()
                 et = time.time() - st
+                lr_scheduler.step()
                 print(f'epoch : {epoch}, loss : {epoch_loss/batch_size}, time : {et}')
+
             torch.save(model, PATH+'DnCNN.pth')
+        elif phase == 'test':
+            pass
+
 
     val_dataloader = test_dataloader
